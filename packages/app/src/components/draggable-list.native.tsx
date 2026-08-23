708b0d8f845586023e8a6349f475eab6a918b907
yooztech <service@yooztech.com>
2026-08-23 15:09:39 +0800

    Fix draggable list gesture host prop

--- packages/app/src/components/draggable-list.native.tsx
+++ packages/app/src/components/draggable-list.native.tsx
@@ -119,6 +119,7 @@ export function DraggableList<T>({
       scrollEnabled={scrollEnabled}
       extraData={extraData}
       simultaneousHandlers={simultaneousHandlers}
+      dragGestureHostPresented={gestureHostPresented}
       // Higher activation distance reduces accidental drag capture while nested
       // lists are inside a scroll container.
       activationDistance={20}
